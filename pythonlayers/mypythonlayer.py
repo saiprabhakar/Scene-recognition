c8dcb2a3d1bcee6e84419ba22e7779be852e2b04
saiprabahakar <aps.prabhakar@gmail.com>
2016-11-11 00:32:26 -0500

    simple training setup and cleaning up using pre-commit

--- pythonlayers/mypythonlayer.py
+++ pythonlayers/mypythonlayer.py
@@ -3,44 +3,47 @@ import numpy as np
 import yaml
 from helpers import _get_image_from_binaryproto, _get_image_list_blob, _get_sim_list_blob
 
-class MyLayer(caffe.Layer):
 
+class MyLayer(caffe.Layer):
     def _parse_file(self, file_name, image_source):
-        f= open(file_name)
+        f = open(file_name)
         lines = [line.rstrip('\n') for line in f]
-        imageList=[]
+        imageList = []
         #import pdb
         #pdb.set_trace()
         for i in lines:
-            temp= i.split(' ')
-            imageList.append( ( image_source+ temp[0], int(temp[1]) ))
+            temp = i.split(' ')
+            imageList.append((image_source + temp[0], int(temp[1])))
         return imageList
-   
+
     def _shuffle_ids(self):
-        self._perm = np.random.permutation( np.arange( self.length_files))
+        self._perm = np.random.permutation(np.arange(self.length_files))
         self._cur = 0
-    
+
     def _get_next_m_batch_ids(self):
-        if self._cur + 2*self.batch_size >= self.length_files:
+        if self._cur + 2 * self.batch_size >= self.length_files:
             self._shuffle_ids()
-        m_batch_ids1 = self._perm[ self._cur: self._cur + self.batch_size]
+        m_batch_ids1 = self._perm[self._cur:self._cur + self.batch_size]
         self._cur += self.batch_size
-        m_batch_ids2 = self._perm[ self._cur: self._cur + self.batch_size]
+        m_batch_ids2 = self._perm[self._cur:self._cur + self.batch_size]
         self._cur += self.batch_size
         return m_batch_ids1, m_batch_ids2
-        
 
     def _get_next_m_batch(self):
         #TODO use prefetch option
         m_batch_ids1, m_batch_ids2 = self._get_next_m_batch_ids()
-        
-        m_batch_1= [self.image_list[i] for i in m_batch_ids1]
-        m_batch_2= [self.image_list[i] for i in m_batch_ids2]
-        blob1= _get_image_list_blob( m_batch_1, self.mean_image, self.scale_min_size, self.final_image_size )
-        blob2= _get_image_list_blob( m_batch_2, self.mean_image, self.scale_min_size, self.final_image_size )
-        blobSim= _get_sim_list_blob( m_batch_1, m_batch_2)
 
-        blobs= {"data":blob1, "data_p":blob2, "sim":blobSim}
+        self.m_batch_1 = [self.image_list[i] for i in m_batch_ids1]
+        self.m_batch_2 = [self.image_list[i] for i in m_batch_ids2]
+        blob1 = _get_image_list_blob(self.m_batch_1, self.mean_image,
+                                     self.scale_min_size,
+                                     self.final_image_size)
+        blob2 = _get_image_list_blob(self.m_batch_2, self.mean_image,
+                                     self.scale_min_size,
+                                     self.final_image_size)
+        blobSim = _get_sim_list_blob(self.m_batch_1, self.m_batch_2)
+
+        blobs = {"data": blob1, "data_p": blob2, "sim": blobSim}
         #TODO verify blob value
         return blobs
 
@@ -51,48 +54,44 @@ class MyLayer(caffe.Layer):
         layer_params = yaml.load(self.param_str)
         #source_params = yaml.load(self.image_str)
         #mean_params = yaml.load(self.mean_str)
-        
+
         self.source_file = layer_params["file_name"]
         self.image_source = layer_params["image_source"]
         self.image_list = self._parse_file(self.source_file, self.image_source)
         self.length_files = len(self.image_list)
-        self._cur =0        
+        self._cur = 0
         self.batch_size = layer_params["batch_size"]
         self._shuffle_ids()
-        
 
         self.final_image_size = layer_params["final_image_size"]
         self.scale_min_size = layer_params["scale_min_size"]
         self.num_channels = layer_params["num_channels"]
-        
-        self.mean_image = _get_image_from_binaryproto( layer_params["mean_image"])
-        assert self.mean_image.shape[1] == self.scale_min_size 
-        assert self.mean_image.shape[0] == self.scale_min_size 
-        assert self.mean_image.shape[2] == self.num_channels 
-        
-        self._name_to_top= {
-          'data': 0,
-          'data_p': 1,
-          'sim': 2}
-        top[0].reshape(1, self.num_channels, self.final_image_size, self.final_image_size)
-        top[1].reshape(1, self.num_channels, self.final_image_size, self.final_image_size)
+
+        self.mean_image = _get_image_from_binaryproto(layer_params[
+            "mean_image"])
+        assert self.mean_image.shape[1] == self.scale_min_size
+        assert self.mean_image.shape[0] == self.scale_min_size
+        assert self.mean_image.shape[2] == self.num_channels
+
+        self._name_to_top = {'data': 0, 'data_p': 1, 'sim': 2}
+        top[0].reshape(1, self.num_channels, self.final_image_size,
+                       self.final_image_size)
+        top[1].reshape(1, self.num_channels, self.final_image_size,
+                       self.final_image_size)
         top[2].reshape(1)
-        
-
-
 
     def reshape(self, bottom, top):
         #if using batch mode do reshaping when calling forward
         pass
 
     def forward(self, bottom, top):
-        print "in forward" 
-        blobs= self._get_next_m_batch()
+        #print "in forward"
+        blobs = self._get_next_m_batch()
         for blob_name, blob in blobs.iteritems():
-          top_ind = self._name_to_top[blob_name]
-          top[top_ind].reshape(*(blob.shape))
-          top[top_ind].data[...] = blob.astype(np.float32, copy=False)
-          #print "top ", blob_name, top[top_ind].data.shape
+            top_ind = self._name_to_top[blob_name]
+            top[top_ind].reshape(*(blob.shape))
+            top[top_ind].data[...] = blob.astype(np.float32, copy=False)
+            #print "top ", blob_name, top[top_ind].data.shape
 
     def backward(self, top, propagate_down, bottom):
         pass
--- siamese/SiameseTrainer.py
+++ siamese/SiameseTrainer.py
@@ -1,10 +1,10 @@
+#/usr/bin/python
 # --------------------------------------------------------
 # Written by Sai Prabhakar, CMU
 # --------------------------------------------------------
 import sys
 import os
 os.environ['GLOG_minloglevel'] = '1'
-
 
 import argparse
 import numpy as np
@@ -22,40 +22,48 @@ from pythonlayers.helpers import im_to_blob
 
 import IPython
 
-im_target_size=227
-
+im_target_size = 227
 
 #TODO: manual training (forward backward)
 #TODO: convert manual training to step function
 
-
 blob = caffe.proto.caffe_pb2.BlobProto()
-data = open( 'placesOriginalModel/places205CNN_mean.binaryproto' , 'rb' ).read()
+data = open('placesOriginalModel/places205CNN_mean.binaryproto', 'rb').read()
 blob.ParseFromString(data)
-arr = np.array( caffe.io.blobproto_to_array(blob) )
-arr= np.squeeze( arr.transpose((2,3,1,0)) )
+arr = np.array(caffe.io.blobproto_to_array(blob))
+arr = np.squeeze(arr.transpose((2, 3, 1, 0)))
 im_scaley = float(im_target_size) / float(256)
 im_scalex = float(im_target_size) / float(256)
-meanarr = cv2.resize(arr, None, None, fx=im_scalex, fy=im_scaley,
-                interpolation=cv2.INTER_LINEAR)
+meanarr = cv2.resize(
+    arr,
+    None,
+    None,
+    fx=im_scalex,
+    fy=im_scaley,
+    interpolation=cv2.INTER_LINEAR)
 
 
 def _get_image_blob(img_name):
-    
+
     im = cv2.imread(img_name)
     im_orig = im.astype(np.float32, copy=True)
-    
-    #im_orig -= 140.8 
-    
+
+    #im_orig -= 140.8
+
     im_shape = im_orig.shape
     im_shape = im_orig.shape
-    im_size = im_shape[0:2] #rows, colmns y,x
-  
+    im_size = im_shape[0:2]  #rows, colmns y,x
+
     im_scaley = float(im_target_size) / float(im_size[0])
     im_scalex = float(im_target_size) / float(im_size[1])
-    im = cv2.resize(im_orig, None, None, fx=im_scalex, fy=im_scaley,
-                    interpolation=cv2.INTER_LINEAR)
-    
+    im = cv2.resize(
+        im_orig,
+        None,
+        None,
+        fx=im_scalex,
+        fy=im_scaley,
+        interpolation=cv2.INTER_LINEAR)
+
     processed_ims = im - meanarr
     #processed_ims = im
     #IPython.embed()
@@ -63,14 +71,16 @@ def _get_image_blob(img_name):
 
     return blob
 
-def _get_blobs(f1,f2,sim):
+
+def _get_blobs(f1, f2, sim):
     """Convert an image and RoIs within that image into network inputs."""
-    blobs = {'data' : None, 'data_p' : None, 'sim' : None}
+    blobs = {'data': None, 'data_p': None, 'sim': None}
     blobs['data'] = _get_image_blob(f1)
     blobs['data_p'] = _get_image_blob(f2)
-    blobs['sim'] = sim.astype(np.float, copy=False) 
+    blobs['sim'] = sim.astype(np.float, copy=False)
     #print "blobs containing", blobs['rois'].shape
     return blobs
+
 
 class SiameseWrapper(object):
     """A simple wrapper around Caffe's solver.
@@ -78,148 +88,187 @@ class SiameseWrapper(object):
     use to unnormalize the learned bounding-box regression weights.
     """
 
-    def __init__(self, solver_prototxt, pretrainaedSiameseModel=None, pretrained_model=None, pretrained_model_proto= None, train= 1 ):
+    def __init__(self,
+                 solver_prototxt,
+                 pretrainaedSiameseModel=None,
+                 pretrained_model=None,
+                 pretrained_model_proto=None,
+                 train=1):
         """Initialize the SolverWrapper."""
         import pdb
         pdb.set_trace()
-        if train==1:
-          self.solver = caffe.SGDSolver(solver_prototxt)
-        else: 
-          self.siameseTestNet= caffe.Net( 'siameseModels/siamesePlaces.prototxt', pretrained_model, caffe.TEST)
-        
-        if pretrainedSiameseModel is not None:
-          print ('Loading pretrained model '
-                 'weights from {:s}').format(pretrainedSiameseModel)
-          if train==1:
-            self.solver.net.copy_from(pretrainedSiameseModel)
-          else:
-            self.siameseTestNet.copy_from(pretrainedSiameseModel)
-            
-        elif pretrained_model is not None:
-          
-          self.caffenet = caffe.Net( pretrained_model_proto, pretrained_model, caffe.TEST)
-          
-          if train==1:
-            self.solver.net.copy_from(pretrained_model)
-          else:
-            self.siameseTestNet.copy_from(pretrained_model)
-          #IPython.embed()
-          #caffenet=[]
-        else :
-          print( 'Initializing completely from scratch .... really ?')
+        if train == 1:
+            self.solver = caffe.SGDSolver(solver_prototxt)
+        else:
+            self.siameseTestNet = caffe.Net(
+                'siameseModels/siamesePlaces.prototxt', pretrained_model,
+                caffe.TEST)
 
-    
-    def testCode (self, fileName) :
-      f= open(fileName)
-      lines = [line.rstrip('\n') for line in f]
-      imageDict={}
-      imlist=[]
-      
-      if train==1:
-        currentNet= self.solver.net
-      else:
-        currentNet= self.siameseTestNet
-      
-      for i in lines:
-        temp= i.split(' ')
-        imageDict[ temp[0] ]= int(temp[1])
-        imlist.append( temp[0] )
-        
-      
-      
-      for i in range( len(imlist)) :
-        for j in range( i, len(imlist)) :
-          sim1=np.array([1])# np.array( [int(imageDict[ imlist[i] ] == imageDict[ imlist[j] ])] )
-      
-          blobs= _get_blobs( 'data/'+ imlist[i], 'data/'+ imlist[j], sim1)
-          blobs_out1 = currentNet.forward( data = blobs['data'].astype(np.float32, copy=False),
-                                data_p = blobs['data_p'].astype(np.float32, copy=False),
-                                sim = blobs['sim'].astype(np.float32, copy=False),
-                                )
-          loss1= blobs_out1[ 'loss' ]
-          print i, j, imageDict[ imlist[i] ], imageDict[ imlist[j] ],  loss1
-        
-      
-      
+        if pretrainedSiameseModel is not None:
+            print('Loading pretrained model '
+                  'weights from {:s}').format(pretrainedSiameseModel)
+            if train == 1:
+                self.solver.net.copy_from(pretrainedSiameseModel)
+            else:
+                self.siameseTestNet.copy_from(pretrainedSiameseModel)
+
+        elif pretrained_model is not None:
+
+            self.caffenet = caffe.Net(pretrained_model_proto, pretrained_model,
+                                      caffe.TEST)
+
+            if train == 1:
+                self.solver.net.copy_from(pretrained_model)
+            else:
+                self.siameseTestNet.copy_from(pretrained_model)
+            #IPython.embed()
+            #caffenet=[]
+        else:
+            print('Initializing completely from scratch .... really ?')
+
+    def testCode(self, fileName):
+        f = open(fileName)
+        lines = [line.rstrip('\n') for line in f]
+        imageDict = {}
+        imlist = []
+
+        if train == 1:
+            currentNet = self.solver.net
+        else:
+            currentNet = self.siameseTestNet
+
+        for i in lines:
+            temp = i.split(' ')
+            imageDict[temp[0]] = int(temp[1])
+            imlist.append(temp[0])
+
+        for i in range(len(imlist)):
+            for j in range(i, len(imlist)):
+                sim1 = np.array([
+                    1
+                ])  # np.array( [int(imageDict[ imlist[i] ] == imageDict[ imlist[j] ])] )
+
+                blobs = _get_blobs('data/' + imlist[i], 'data/' + imlist[j],
+                                   sim1)
+                blobs_out1 = currentNet.forward(
+                    data=blobs['data'].astype(
+                        np.float32, copy=False),
+                    data_p=blobs['data_p'].astype(
+                        np.float32, copy=False),
+                    sim=blobs['sim'].astype(
+                        np.float32, copy=False), )
+                loss1 = blobs_out1['loss']
+                print i, j, imageDict[imlist[i]], imageDict[imlist[j]], loss1
+
+
 class SiameseTrainWrapper2(object):
     """A simple wrapper around Caffe's solver.
     This wrapper gives us control over he snapshotting process, which we
     use to unnormalize the learned bounding-box regression weights.
     """
 
-    def __init__(self, solver_prototxt, pretrainedSiameseModel=None, pretrained_model=None, pretrained_model_proto= None, train=1):
+    def __init__(self,
+                 solver_prototxt,
+                 pretrainedSiameseModel=None,
+                 pretrained_model=None,
+                 pretrained_model_proto=None,
+                 train=1):
         """Initialize the SolverWrapper."""
-        
 
-        if train==1:
-          self.solver = caffe.SGDSolver(solver_prototxt)
-        else: 
-          self.siameseTestNet= caffe.Net( 'siameseModels/siamesePlaces.prototxt', pretrained_model, caffe.TEST)
-        
+        if train == 1:
+            self.solver = caffe.SGDSolver(solver_prototxt)
+        else:
+            self.siameseTestNet = caffe.Net(
+                'siameseModels/siamesePlaces.prototxt', pretrained_model,
+                caffe.TEST)
+
         if pretrainedSiameseModel is not None:
-          print ('Loading pretrained model '
-                 'weights from {:s}').format(pretrainedSiameseModel)
-          if train==1:
-            self.solver.net.copy_from(pretrainedSiameseModel)
-          else:
-            self.siameseTestNet.copy_from(pretrainedSiameseModel)
-            
-        elif pretrained_model is not None:
-          
-          self.caffenet = caffe.Net( pretrained_model_proto, pretrained_model, caffe.TEST)
-          
-          if train==1:
-            self.solver.net.copy_from(pretrained_model)
-          else:
-            self.siameseTestNet.copy_from(pretrained_model)
-          #IPython.embed()
-          #caffenet=[]
-        else :
-          print( 'Initializing completely from scratch .... really ?')
+            print('Loading pretrained model '
+                  'weights from {:s}').format(pretrainedSiameseModel)
+            if train == 1:
+                self.solver.net.copy_from(pretrainedSiameseModel)
+            else:
+                self.siameseTestNet.copy_from(pretrainedSiameseModel)
 
-    
-    def testCode (self) :
-      '''f= open(fileName)
+        elif pretrained_model is not None:
+
+            self.caffenet = caffe.Net(pretrained_model_proto, pretrained_model,
+                                      caffe.TEST)
+
+            if train == 1:
+                self.solver.net.copy_from(pretrained_model)
+            else:
+                self.siameseTestNet.copy_from(pretrained_model)
+            #IPython.embed()
+            #caffenet=[]
+        else:
+            print('Initializing completely from scratch .... really ?')
+
+    def testCode(self):
+        '''f= open(fileName)
       lines = [line.rstrip('\n') for line in f]
       imageDict={}
       imlist=[]
-      
+
       if train==1:
         currentNet= self.solver.net
       else:
         currentNet= self.siameseTestNet
-      
+
       for i in lines:
         temp= i.split(' ')
         imageDict[ temp[0] ]= int(temp[1])
         imlist.append( temp[0] )'''
-        
-      #import pdb
-      #pdb.set_trace()
-        
-      
-      for i in range( 2) :
-          self.solver.step(1)
-          loss1= self.solver.net.blobs['loss']
-          #print i,  loss1
-        
-      
-      
-      
-def siameseTrainer(siameseSolver, fileName, pretrained_model, pretrained_model_proto):
+
+        #import ipdb
+        #ipdb.set_trace()
+
+        for k in range(10):
+            simLoss = 0
+            disLoss = 0
+            simC = 0
+            disC = 0
+            for i in range(30):
+                self.solver.step(1)
+                loss1 = self.solver.net.blobs['loss'].data
+                #self.solver.net.layers[0].source_file
+
+                #import IPython
+                #IPython.embed()
+                if self.solver.net.blobs['sim'].data == 1:
+                    simLoss += self.solver.net.blobs['loss'].data
+                    simC += 1
+                else:
+                    disLoss += self.solver.net.blobs['loss'].data
+                    disC += 1
+                print i, loss1, self.solver.net.blobs['sim'].data
+            print "**** net loss", simLoss / simC, disLoss / disC
+        '''for j in range(len(self.solver.net.blobs['sim'].data)):
+                import IPython
+                IPython.embed()
+                if self.solver.net.blobs['sim'].data[j] == 0:
+                    simLoss+= self.solver.net.blobs['loss'].data[j]
+                    simC+=1
+                else:
+                    disLoss+= self.solver.net.blobs['loss'].data[j]
+                    disC+=1'''
+
+
+def siameseTrainer(siameseSolver, fileName, pretrained_model,
+                   pretrained_model_proto):
     """Test a Fast R-CNN network on an image database."""
-    numImagePair = 1#len(imdb.image_index)
+    numImagePair = 1  #len(imdb.image_index)
     # timers
     #_t = {'im_detect' : Timer(), 'misc' : Timer()}
-    
-    sw = SiameseTrainWrapper2(siameseSolver, pretrained_model=pretrained_model, pretrained_model_proto= pretrained_model_proto, train=1 )
-    
+
+    sw = SiameseTrainWrapper2(
+        siameseSolver,
+        pretrained_model=pretrained_model,
+        pretrained_model_proto=pretrained_model_proto,
+        train=1)
+
     sw.testCode()
-    
+
     #sw = SiameseWrapper(siameseSolver, pretrained_model=pretrained_model, pretrained_model_proto= pretrained_model_proto, train=0 )
-    
+
     #sw.testCode( fileName)
-    
-    
-    
-    
